b1c780e59646269fe4acf60f34f4aec4cc81b14a
Leeron Luzzatto <lluzzatto@microsoft.com>
2022-07-23 12:51:38 +0300

    Fix Data Paths
    This commit replaces the '\' char with '/' so the new paths are valid
    also for unix.

--- DataLoaders/Concrete/SpotPriceDataLoader.py
+++ DataLoaders/Concrete/SpotPriceDataLoader.py
@@ -5,7 +5,7 @@ from DataLoaders.Abstract.AbstractAwsDataLoader import AbstractAwsDataLoader
 
 class SpotPriceDataLoader(AbstractAwsDataLoader):
     url = 'http://spot-price.s3.amazonaws.com/spot.js'
-    data_files_folder = r'data\SpotPrice'
+    data_files_folder = r'data/SpotPrice'
     data_file_name_format = 'spot-price-data-{date}.json'
 
     @staticmethod
